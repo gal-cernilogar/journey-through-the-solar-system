05d2294d121e4410647d9880ab1fd8011d403eaa
Gal Černilogar <gal.cernilogar@outfit7.com>
2023-08-11 13:43:13 +0200

    Remove boilerplate

--- shaders/coronaShader/fragment.glsl
+++ shaders/coronaShader/fragment.glsl
@@ -1,13 +1,4 @@
 varying vec3 vPosition;
-varying vec3 vNormal;
-varying vec3 eyeVector;
-varying vec3 vLayer0;
-varying vec3 vLayer1;
-varying vec3 vLayer2;
-
-float Fresnel(vec3 eyeVector, vec3 worldNormal) {
-  return pow(1.0 + dot(eyeVector, worldNormal), 3.0);
-}
 
 vec3 brightnessToColor(float b) {
   b *= 0.25;
